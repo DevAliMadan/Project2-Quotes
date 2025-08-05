0007811008d872b8d54ee9fe13733c49c38a0e91
Ali Madan <alimadan4321@gmail.com>
2025-08-05 08:15:35 +0300

    did the login and sign-up css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/*.{html,js,css}",
-    "./views/*.ejs",
-  ],
+  content: ["./views/**/*.ejs"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
